6a34978ca4f51b25737dd45190bf59b5c1032a9b
shives <87254612+shives@users.noreply.github.com>
2022-08-11 16:16:51 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function startClassification()
-{
-    navigator.mediaDevices.getUserMedia({ audio: true});
-    classifier = ml5.soundClassifier('', modelReady);
-}
-
-function modelReady(){
-    classifier.classify(gotResults);
-}
